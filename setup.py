987949135b1cc7660f2193b80fd4343028b5097d
Florian Zwagemaker <florian@niqex.com>
2021-03-23 22:55:25 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,36 @@
+import sys
+
+if sys.version_info.major != 3 or sys.version_info.minor < 7:
+    print("Error: you must execute setup.py using Python 3.7 or later")
+    sys.exit(1)
+
+from setuptools import setup, find_packages
+from AmpliGone.version import __version__
+#exec(open("AmpliGone/version.py").read())
+
+with open("README.md", "rb") as readme:
+    DESCR = readme.read().decode()
+
+
+setup(
+    name="AmpliGone",
+    version=__version__,
+    author='Florian Zwagemaker',
+    author_email='rivm-bioinformatics@rivm.nl',
+    license='AGPLv3',
+    packages=find_packages(),
+    install_requires=[
+        'pysam>=0.16',
+        'pandas>=1.2.3',
+        'numpy>=1.20'
+        'mappy>=2.17',
+        'biopython>=1.78',
+        'parmap>=1.5.2',
+        'tqdm>=4.59.0'
+    ],
+    entry_points={"console_scripts": [
+        'ampligone = AmpliGone.AmpliGone:main',
+        'AmpliGone = AmpliGone.AmpliGone:main']},
+    keywords=[],
+    zip_safe=False
+)
