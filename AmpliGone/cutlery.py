b04a85675eb9657083c5b40e4ce6ce10e1d696fa
Florian Zwagemaker <florian@niqex.com>
2021-03-25 15:05:05 +0100

    Improve formatting

--- AmpliGone/cutlery.py
+++ AmpliGone/cutlery.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 
+
 @lru_cache(maxsize=None)
 def ReadBeforePrimer(pos, clist):
     if pos in clist:
@@ -11,6 +12,7 @@ def ReadBeforePrimer(pos, clist):
     else:
         return False
 
+
 @lru_cache(maxsize=None)
 def ReadAfterPrimer(pos, clist):
     if pos in clist:
@@ -21,35 +23,39 @@ def ReadAfterPrimer(pos, clist):
         return True
     else:
         return False
-    
+
+
 @lru_cache(maxsize=600000)
 def slice_fw_left(start, seq, qual):
     start = start + 1
     tseq = seq[1:]
     tqual = qual[1:]
-    
+
     return tseq, tqual, start
+
 
 @lru_cache(maxsize=600000)
 def slice_fw_right(end, seq, qual):
     end = end - 1
     tseq = seq[:-1]
     tqual = qual[:-1]
-    
+
     return tseq, tqual, end
+
 
 @lru_cache(maxsize=600000)
 def slice_rv_left(start, seq, qual):
     start = start + 1
     tseq = seq[:-1]
     tqual = qual[:-1]
-    
+
     return tseq, tqual, start
+
 
 @lru_cache(maxsize=600000)
 def slice_rv_right(end, seq, qual):
     end = end - 1
     tseq = seq[1:]
     tqual = qual[1:]
-    
+
     return tseq, tqual, end
--- AmpliGone/io_ops.py
+++ AmpliGone/io_ops.py
@@ -13,29 +13,34 @@ def is_zipped(filename):
         return True
     else:
         return False
-    
+
+
 def is_fastq(filename):
     fqextenstions = [".fastq", ".fq"]
     if any(item in fqextenstions for item in pathlib.Path(filename).suffixes) is True:
         return True
     else:
         return False
-    
+
+
 def is_bam(filename):
     if ".bam" in pathlib.Path(filename).suffixes:
         return True
     else:
         return False
 
+
 def read_gzip(filename):
-    with gzip.open(filename, 'rb') as f:
+    with gzip.open(filename, "rb") as f:
         lines = f.read()
     return lines
 
+
 def read_fastq(filename):
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         lines = f.read()
     return lines
+
 
 def IO_handle(inputfile):
     if is_fastq(inputfile) is True:
@@ -49,9 +54,12 @@ def IO_handle(inputfile):
         bamfile = pysam.AlignmentFile(inputfile, "rb")
         return bamfile
     else:
-        print(f"\"{inputfile}\" is an unsupported filetype. Please try again with a supported filetype")
+        print(
+            f'"{inputfile}" is an unsupported filetype. Please try again with a supported filetype'
+        )
         sys.exit(-1)
-        
+
+
 def FlipStrand(seq, qual):
     complement = {"A": "T", "C": "G", "G": "C", "T": "A", "N": "N"}
     bases = list(seq)
@@ -59,8 +67,9 @@ def FlipStrand(seq, qual):
     seq = "".join(bases)
     seq = seq[::-1]
     qual = qual[::-1]
-    
+
     return seq, qual
+
 
 def ReadsToDict(inputfile):
     ReadDict = {}
@@ -69,10 +78,10 @@ def ReadsToDict(inputfile):
     if is_fastq(inputfile) is True:
         handle = IO_handle(inputfile)
         for record in SeqIO.parse(handle, "fastq"):
-            
+
             RecordQualities = "".join(
                 map(lambda x: chr(x + 33), record.letter_annotations["phred_quality"])
-                )
+            )
             seq = record.seq
             if len(seq) == 0:
                 seq = np.nan
@@ -80,9 +89,9 @@ def ReadsToDict(inputfile):
             ReadDict[i] = {
                 "Readname": str(record.id),
                 "Sequence": str(seq),
-                "Qualities": str(RecordQualities)
-                }
-            
+                "Qualities": str(RecordQualities),
+            }
+
             i = i + 1
         handle.close()
     elif is_bam(inputfile) is True:
@@ -90,27 +99,29 @@ def ReadsToDict(inputfile):
         for read in handle:
             if read.is_unmapped is True:
                 continue
-            
+
             readname = read.query_name
             ReadSeq = read.query_sequence
-            RecordQualities = ''.join(map(lambda x: chr( x+33 ), read.query_qualities))
-            
+            RecordQualities = "".join(map(lambda x: chr(x + 33), read.query_qualities))
+
             if read.is_reverse is True:
                 ReadSeq, RecordQualities = FlipStrand(ReadSeq, RecordQualities)
 
             if len(ReadSeq) == 0:
                 ReadSeq = np.nan
-            
+
             ReadDict[i] = {
-                "Readname": str(readname), 
-                "Sequence": str(ReadSeq), 
-                "Qualities": str(RecordQualities)
-                }
-            
+                "Readname": str(readname),
+                "Sequence": str(ReadSeq),
+                "Qualities": str(RecordQualities),
+            }
+
             i = i + 1
-    
+
     else:
-        print(f"\"{inputfile}\" is an unsupported filetype. Please try again with a supported filetype")
+        print(
+            f'"{inputfile}" is an unsupported filetype. Please try again with a supported filetype'
+        )
         sys.exit(-1)
     return ReadDict
 
@@ -136,13 +147,15 @@ def ReadsToList(inputfile):
             quals = read.query_qualities.tolist()
             for q in quals:
                 QualList.append(q)
-                
-                
+
             ReadList.append(str(ReadSeq))
     else:
-        print(f"\"{inputfile}\" is an unsupported filetype. Please try again with a supported filetype")
+        print(
+            f'"{inputfile}" is an unsupported filetype. Please try again with a supported filetype'
+        )
         sys.exit(-1)
     return ReadList, QualList
+
 
 def IndexReads(inputfile):
     ReadIndex = pd.DataFrame.from_dict(ReadsToDict(inputfile), "index")
@@ -150,19 +163,13 @@ def IndexReads(inputfile):
 
 
 def WriteOutput(output, ReadDict):
-    with open(output, 'w') as fileout:
+    with open(output, "w") as fileout:
         for index in range(len(ReadDict)):
             for key in ReadDict[index]:
                 if key == "Readname":
-                    fileout.write(
-                        "@" + ReadDict[index][key] + "\n"
-                    )
-                if key == 'Sequence':
-                    fileout.write(
-                        str(ReadDict[index][key]) + "\n" + "+" + "\n"
-                    )
-                if key == 'Qualities':
-                    fileout.write(
-                        str(ReadDict[index][key]) + "\n"
-                    )
+                    fileout.write("@" + ReadDict[index][key] + "\n")
+                if key == "Sequence":
+                    fileout.write(str(ReadDict[index][key]) + "\n" + "+" + "\n")
+                if key == "Qualities":
+                    fileout.write(str(ReadDict[index][key]) + "\n")
     pass
