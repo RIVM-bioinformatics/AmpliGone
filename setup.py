d1617ae621af2ca88dfd06cd73204bcd8b8b0809
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-03-14 15:59:37 +0100

    build: update setup.py for package publication

--- setup.py
+++ setup.py
@@ -8,15 +8,20 @@ if sys.version_info.major != 3 or sys.version_info.minor < 7:
     print("Error: you must execute setup.py using Python 3.7 or later")
     sys.exit(1)
 
-with open("README.md", "rb") as readme:
-    DESCR = readme.read().decode()
-
+with open("README.md", "r", encoding="utf-8") as readme:
+    DESCR = readme.read()
 
 setup(
     name="AmpliGone",
     version=__version__,
+    url="https://rivm-bioinformatics.github.io/AmpliGone/",
+    project_urls={"Source Code": "https://github.com/RIVM-bioinformatics/AmpliGone"},
     author="Florian Zwagemaker",
     author_email="ids-bioinformatics@rivm.nl",
+    description="Ampligone is a tool which accurately removes primer sequences from FastQ NGS reads in an amplicon sequencing experiment",
+    long_description=DESCR,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
     license="AGPLv3",
     packages=find_packages(),
     install_requires=[
@@ -37,4 +42,11 @@ setup(
     },
     keywords=[],
     zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Intended Audience :: Science/Research",
+    ],
 )
