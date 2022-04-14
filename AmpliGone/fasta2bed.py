ea2d5c6b09bf976b052e3c4d716a0ca216c785e8
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-04-14 11:01:47 +0200

    docs: add docstrings to functions in AmpliGone.fasta2bed

--- AmpliGone/fasta2bed.py
+++ AmpliGone/fasta2bed.py
@@ -7,11 +7,41 @@ from Bio import Seq, SeqIO
 
 
 def FindAmbigousOptions(seq):
+    """It takes a sequence containing ambiguous nucleotides and returns a list of all possible unambiguous
+    sequences
+
+    Parameters
+    ----------
+    seq
+        The sequence to be searched for.
+
+    Returns
+    -------
+        A list of all possible combinations of the nucleotides in the sequence without ambiguous options.
+
+    """
     ambigs = Seq.IUPACData.ambiguous_dna_values
     return list(map("".join, product(*map(ambigs.get, seq))))
 
 
 def get_coords(seq, ref_seq, err_rate=0.1):
+    """Finds all the positions in the reference sequence where the query sequence could be found, allowing
+    for up to `err_rate` errors
+
+    Parameters
+    ----------
+    seq
+        The sequence you want to find in the reference sequence.
+    ref_seq
+        The reference sequence to search in.
+    err_rate
+        The maximum error rate you want to allow.
+
+    Returns
+    -------
+        A set of tuples, each tuple is a match of the sequence in the reference sequence.
+
+    """
     max_errors = int(len(seq) * err_rate)
 
     options = FindAmbigousOptions(seq.upper())
@@ -27,6 +57,13 @@ def get_coords(seq, ref_seq, err_rate=0.1):
 
 
 def MakeCoordinateLists(*args, **kwargs):
+    """Takes a list of sequences, and returns a list of coordinates for each sequence
+
+    Returns
+    -------
+        A dataframe with the columns ref, start, end, name, score, strand, seq, revcomp
+
+    """
     return pd.DataFrame(
         CoodListGen(*args, **kwargs),
         columns=["ref", "start", "end", "name", "score", "strand", "seq", "revcomp"],
@@ -34,6 +71,19 @@ def MakeCoordinateLists(*args, **kwargs):
 
 
 def CoodListGen(primerfile, referencefile, err_rate=0.1):
+    """Takes a fasta file of primers and a fasta file of a reference sequence, and returns a list of
+    dictionaries containing the coordinates of the primers in the reference sequence
+
+    Parameters
+    ----------
+    primerfile
+        The file containing the primers.
+    referencefile
+        The reference file that you want to search for the primers in.
+    err_rate
+        The maximum number of mismatches allowed between the primer and the reference.
+
+    """
     keyl = ("LEFT", "PLUS", "POSITIVE", "FORWARD")
     keyr = ("RIGHT", "MINUS", "NEGATIVE", "REVERSE")
 
@@ -88,6 +138,21 @@ def CoodListGen(primerfile, referencefile, err_rate=0.1):
 
 
 def CoordinateListsToBed(df, outfile):
+    """It takes a dataframe with columns named "ref", "start", "end", "name", "score", and "strand" and
+    writes them to a file in BED (headerless tsv) format
+
+    Parameters
+    ----------
+    df
+        the dataframe containing the coordinates
+    outfile
+        the name of the file to write to
+
+    Returns
+    -------
+        A dataframe with the columns ref, start, end, name, score, and strand.
+
+    """
     return df[["ref", "start", "end", "name", "score", "strand"]].to_csv(
         outfile, sep="\t", na_rep=".", header=False, index=False
     )
