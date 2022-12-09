c8af27a995bc04296e3553e437642af4297ce4f9
Florian Zwagemaker <f.zwagemaker@gmail.com>
2022-12-09 09:58:59 +0100

    docs: update docstrings

--- AmpliGone/cutlery.py
+++ AmpliGone/cutlery.py
@@ -2,47 +2,53 @@ from functools import lru_cache
 
 
 @lru_cache(maxsize=2000000)
-def PositionInOrBeforePrimer(pos, clist, max_lookback):
-    """Given a position and a list of positions, return True if the position is in the list or is before
-    the nearest position in the list
+def PositionInOrBeforePrimer(pos, clist, max_lookaround):
+    """Given a position, a list of positions, and a maximum distance, return True if the position is
+    within the maximum distance of the closest position in the list AND the position is less than or
+    equal to the closest position in the list
 
     Parameters
     ----------
     pos
-        the position on a sequence query
+        the position of the variant
     clist
-        a tuple of integers, the positions of the primers
+        a list of primer positions
+    max_lookaround
+        the maximum distance from the position to the closest primer
 
     Returns
     -------
-        Boolean value
+        A boolean value.
 
     """
     d = lambda x: abs(x - pos)
     near = min(clist, key=d)
-    return abs(pos - near) < max_lookback and pos <= near
+    return abs(pos - near) < max_lookaround and pos <= near
 
 
 @lru_cache(maxsize=2000000)
-def PositionInOrAfterPrimer(pos, clist, max_lookback):
-    """Given a position and a list of positions, return True if the position is in the list or after the
-    closest position in the list
+def PositionInOrAfterPrimer(pos, clist, max_lookaround):
+    """Given a position, a list of positions, and a maximum distance, return True if the position is
+    within the maximum distance of the closest position in the list AND the position is greater than or equal
+    to the closest position in the list
 
     Parameters
     ----------
     pos
-        the position on a sequence query
+        the start or stop position of the read
     clist
-        a tuple of integers, the positions of the primers
+        a list of primer positions
+    max_lookaround
+        the maximum distance from the position to the closest primer
 
     Returns
     -------
-        Boolean value
+        A boolean value.
 
     """
     d = lambda x: abs(x - pos)
     near = min(clist, key=d)
-    return abs(pos - near) < max_lookback and pos >= near
+    return abs(pos - near) < max_lookaround and pos >= near
 
 
 @lru_cache(maxsize=2000000)
