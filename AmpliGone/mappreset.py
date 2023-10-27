7e1c8f4576d7d24586871f53d995fa3bf427f225
Florian Zwagemaker <f.zwagemaker@gmail.com>
2023-10-27 15:24:40 +0200

    refactor: add type hints to all functions in mappreset.py
    refactor: add inline type declarations where necessary or helpful in mappreset.py
    
    docs: update docstrings of functions in mappreset.py

--- AmpliGone/mappreset.py
+++ AmpliGone/mappreset.py
@@ -1,159 +1,223 @@
 from concurrent.futures import ThreadPoolExecutor
+from typing import List, Tuple
+
+import pandas as pd
 
 
-def Calculate_avg_seq_len(Slist):
-    """Takes a list of sequences, and returns the average length of the sequences
+def Calculate_avg_seq_len(sequence_list: List[str]) -> float:
+    """
+    Calculate the average length of a list of sequences.
 
     Parameters
     ----------
-    Slist
-        a list of sequences
+    sequence_list : List[str]
+        A list of sequences.
 
     Returns
     -------
+    float
         The average length of the sequences in the list.
 
+    Examples
+    --------
+    >>> Calculate_avg_seq_len(['ATCG', 'ATCGATCG', 'AT'])
+    5.0
     """
-    return sum(map(len, Slist)) / float(len(Slist))
+    return sum(map(len, sequence_list)) / float(len(sequence_list))
 
 
-def Calculate_avg_seq_qual(Qlist):
-    """This function takes a list of quality scores and returns the average quality score
+def Calculate_avg_seq_qual(quality_list: List[int]) -> float:
+    """
+    Calculate the average quality score of a list of quality scores.
 
     Parameters
     ----------
-    Qlist
-        a list of quality scores
+    quality_list : List[int]
+        A list of quality scores.
 
     Returns
     -------
-        The average quality score of the sequence.
+    float
+        The average quality score of the quality scores in the list.
 
+    Examples
+    --------
+    >>> Calculate_avg_seq_qual([20, 30, 40])
+    30.0
     """
-    return sum(Qlist) / len(Qlist)
+    return sum(quality_list) / len(quality_list)
 
 
-def GetQualRange(Qlist):
-    """Takes a list of quality scores, converts them to a set, and returns the length of that set
+def GetQualRange(quality_list: List[int]) -> int:
+    """
+    Return the number of unique quality scores in a list.
 
     Parameters
     ----------
-    Qlist
-        a list of quality scores
+    quality_list : List[int]
+        A list of quality scores.
 
     Returns
     -------
-        The number of unique values in the list.
+    int
+        The number of unique quality scores in the list.
 
+    Examples
+    --------
+    >>> GetQualRange([20, 30, 20, 40])
+    3
     """
-    return len(list(set(Qlist)))
+    return len(set(quality_list))
 
 
-def GetLenRange(SList):
-    """Takes a list of strings, and returns the number of different lengths of strings in the list
+def GetLenRange(sequence_list: List[str]) -> int:
+    """
+    Return the number of unique lengths of strings in a list.
 
     Parameters
     ----------
-    SList
+    sequence_list : List[str]
         A list of strings.
 
     Returns
     -------
+    int
         The number of unique lengths in the list of strings.
 
+    Examples
+    --------
+    >>> GetLenRange(['ATCG', 'ATCGATCG', 'AT'])
+    2
     """
-    AllLengths = [len(i) for i in SList]
-    return len(list(set(AllLengths)))
+    AllLengths = [len(i) for i in sequence_list]
+    return len(set(AllLengths))
 
 
-def SequenceVariability(diffQuals, difflen, avg_qual, avg_len):
-    """The function takes the difference between the average quality score and the quality score of the
+def SequenceVariability(
+    QualRange: int, LengthRange: int, avg_qual: float, avg_len: float
+) -> Tuple[float, float]:
+    """
+    Calculate the variability of a sequence based on the difference between the average quality score and the quality score of the
     current sequence, and the difference between the average length and the length of the current
-    sequence, and returns the ratio of these differences to the average quality score and average
-    length, respectively.
+    sequence.
 
     Parameters
     ----------
-    diffQuals
-        the difference between the highest and lowest quality scores
-    difflen
-        the difference between the longest and shortest sequence
-    avg_qual
-        average quality score of the sequence
-    avg_len
-        average length of the sequences
+    QualRange : int
+        The difference between the highest and lowest quality scores.
+    LengthRange : int
+        The difference between the longest and shortest sequence.
+    avg_qual : float
+        The average quality score of the sequence.
+    avg_len : float
+        The average length of the sequences.
 
     Returns
     -------
-        the quality variance and length variance.
+    Tuple[float, float]
+        The quality variance and length variance.
 
+    Examples
+    --------
+    >>> SequenceVariability(20, 10, 30.0, 5.0)
+    (0.6666666666666666, 2.0)
     """
-    QualVariance = diffQuals / avg_qual
-    LengthVariance = difflen / avg_len
+    QualVariance = QualRange / avg_qual
+    LengthVariance = LengthRange / avg_len
     return QualVariance, LengthVariance
 
 
-def SequenceStability(QualRange, LengthRange, avg_qual, avg_len):
-    """Function takes in the range of quality scores and the range of sequence lengths, and returns the
-    stability of the sequences as a percentage.
+def SequenceStability(
+    QualRange: int, LengthRange: int, avg_qual: float, avg_len: float
+) -> float:
+    """
+    Calculate the stability of a sequence based on the variability of the quality scores and the lengths of the sequences.
 
     Parameters
     ----------
-    QualRange
-        The range of quality scores in the sequence.
-    LengthRange
-        The range of lengths of the sequences in the file.
-    avg_qual
-        average quality score of the sequence
-    avg_len
-        average length of the sequences
+    QualRange : int
+        The difference between the highest and lowest quality scores.
+    LengthRange : int
+        The difference between the longest and shortest sequence.
+    avg_qual : float
+        The average quality score of the sequence.
+    avg_len : float
+        The average length of the sequences.
 
     Returns
     -------
+    float
         The percentage of stability of the sequence.
 
+    Examples
+    --------
+    >>> SequenceStability(20, 10, 30.0, 5.0)
+    32.33333333333333
     """
     QualVar, LenVar = SequenceVariability(QualRange, LengthRange, avg_qual, avg_len)
     return 100 - (QualVar + LenVar)
 
 
-def IsLongRead(avg_len):
-    """If the average read length is greater than 300, then the read is considered to be long
+def IsLongRead(avg_len: float) -> bool:
+    """
+    Determine if a read is considered long based on its average length.
 
     Parameters
     ----------
-    avg_len
-        average read length
+    avg_len : float
+        The average length of the read.
 
     Returns
     -------
-        A boolean value
+    bool
+        True if the read is considered long (average length > 300), False otherwise.
 
+    Examples
+    --------
+    >>> IsLongRead(250)
+    False
+    >>> IsLongRead(350)
+    True
     """
     return avg_len > 300
 
 
-def FindPreset(threads, data):
-    """Takes a dataframe with the sequence and quality data, and returns a preset name and a list of
-    scoring parameters
+def FindPreset(threads: int, data: pd.DataFrame) -> Tuple[str, List[int]]:
+    """
+    Takes a dataframe with the sequence and quality data, and returns a preset name and a list of scoring parameters.
 
     Parameters
     ----------
-    threads
-        number of threads to use for the calculations
-    data
-        a pandas dataframe containing the reads and qualities
+    threads : int
+        Number of threads to use for the calculations.
+    data : pandas.DataFrame
+        A dataframe containing the reads and qualities.
 
     Returns
     -------
-        a tuple of two values.
-    The first value is a string that represents the preset.
-    The second value is a list of integers that represent the scoring matrix.
+    Tuple[str, List[int]]
+        A tuple of two values. The first value is a string that represents the preset. The second value is a list of integers that represent the scoring matrix.
 
+    Raises
+    ------
+    None
+
+    Examples
+    --------
+    >>> data = pd.DataFrame({'Sequence': ['ATCG', 'GCTA'], 'Qualities': ['!@#$', 'abcd']})
+    >>> FindPreset(threads=2, data=data)
+    ('sr', [])
+
+    Notes
+    -----
+    This function takes a dataframe with the sequence and quality data, and returns a preset name and a list of scoring parameters.
+    It first extracts the sequence and quality data from the dataframe, and then calculates the average length and quality of the sequences,
+    as well as the range of quality scores and sequence lengths. Based on these values, it determines the appropriate preset to use for the data.
+    If the data is considered stable and short, it returns the 'sr' preset. If the data is considered stable and long, it returns the 'map-ont' preset.
+    If the data is considered unstable or from an unsupported platform, it falls back to the 'sr' preset.
     """
-
-    ReadList = data["Sequence"].tolist()
-    QualList = [
+    ReadList: List[str] = data["Sequence"].tolist()
+    QualList: List[int] = [
         ord(character) - 33
         for character in [
             x for y in [list(item) for item in data["Qualities"].tolist()] for x in y
